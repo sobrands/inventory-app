f1e8e4d7c80c107c80a7c5a8eb2af013a20f2915
brandon <lim.wx.brandon@gmail.com>
2024-07-31 23:02:44 +0800

    Transitioning from MongoDB to Postgres
    So far populatedb done, now left to redo controllers

--- populatedb.js
+++ populatedb.js
@@ -1,114 +1,73 @@
 require("dotenv").config();
+const { Client } = require("pg");
 
-const Recipe = require("./models/recipe");
-const FilmSim = require("./models/film-sim");
-const Source = require("./models/source");
+const SQL = `
+CREATE TABLE IF NOT EXISTS sources (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  description TEXT
+);
 
-const filmSims = [];
-const sources = [];
+CREATE TABLE IF NOT EXISTS film_sims (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL
+);
 
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+CREATE TABLE IF NOT EXISTS recipes (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  film_sim INTEGER REFERENCES film_sims (id),
+  dynamic_range TEXT NOT NULL,
+  grain TEXT NOT NULL,
+  ccfx TEXT NOT NULL,
+  ccfx_blue TEXT NOT NULL,
+  highlight INTEGER NOT NULL,
+  shadow INTEGER NOT NULL,
+  wb_style TEXT NOT NULL,
+  wb_shift_red INTEGER NOT NULL,
+  wb_shift_blue INTEGER NOT NULL,
+  sharpness INTEGER NOT NULL,
+  noise_reduction INTEGER NOT NULL,
+  clarity INTEGER NOT NULL,
+  iso INTEGER NOT NULL,
+  exposure_compensation_min TEXT NOT NULL,
+  exposure_compensation_max TEXT NOT NULL,
+  source INTEGER REFERENCES sources (id),
+  reference_url TEXT
+);
 
-const mongoDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wvjemnw.mongodb.net/inventory-app?retryWrites=true&w=majority&appName=Cluster0`
+INSERT INTO sources (name, description)
+VALUES 
+  ('FujiXWeekly', 'Goated Recipe Platform'),
+  ('CaptnLook', 'Cinematic Film Presets');
 
-main().catch((err) => console.log(err));
+INSERT INTO film_sims (name)
+VALUES 
+  ('Classic Chrome'),
+  ('Nostalgic Negative'),
+  ('Classic Negative');
+
+INSERT INTO recipes (name, film_sim, dynamic_range, grain,
+  ccfx, ccfx_blue, wb_style, wb_shift_red, wb_shift_blue,
+  highlight, shadow, sharpness, noise_reduction, clarity, iso,
+  exposure_compensation_min, exposure_compensation_max, source
+)
+VALUES
+  ('Kodak Gold 200', (SELECT id FROM film_sims WHERE name = 'Classic Chrome'),
+   'DR-Auto', 'Strong', 'Off', 'Off', 'Daylight', 4, -5, -2, 1, -2, -4, 0,
+   6400, '2/3', '1 1/3', (SELECT id FROM sources WHERE name = 'FujiXWeekly')
+  );
+`
 
 async function main() {
-  console.log("About to connect to MongoDB...");
-  await mongoose.connect(mongoDB);
-  console.log("Connected!");
-  await createFilmSims();
-  await createSources();
-  await createRecipes();
-  console.log("Closing mongoose");
-  mongoose.connection.close();
-}
-
-async function filmSimCreate(index, name) {
-  // Check if filmSim exists in DB first
-  const filmSim = await FilmSim.find({ name: name }).exec();
-
-  if (filmSim.length > 0) {
-    console.log(`${name} already exists!`);
-  } else {
-    const sim = new FilmSim({ name: name });
-    await sim.save();
-    filmSims[index] = sim;
-    console.log(`Added Film Simulation: ${name}`);
-  } 
-}
-
-async function sourceCreate(index, name, desc) {
-  // Check if source exists in DB first
-  const source = await Source.find({ name: name }).exec();
-
-  if (source.length > 0) {
-    console.log(`${name} already exists!`);
-  } else {
-    const src = new Source({
-      name: name,
-      description: desc
-    });
-    await src.save();
-    sources[index] = src;
-    console.log(`Added source: ${name}`);
-  }
-}
-
-async function recipeCreate(index, name, filmSim, settings, source) {
-  const recipe = new Recipe({
-    name: name,
-    film_sim: filmSim,
-    settings: settings,
-    source: source
+  console.log("About to connect to Postgres DB...");
+  const client = new Client({
+    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/inventory_app`
   });
-  await recipe.save();
-  console.log(`Added recipe: ${name}`);
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
 }
 
-async function createFilmSims() {
-  console.log("Adding film simulations");
-  await Promise.all([
-    filmSimCreate(0, "Classic Chrome"),
-    filmSimCreate(1, "Nostalgic Negative"),
-    filmSimCreate(2, "Classic Negative")
-  ]);
-}
-
-async function createSources() {
-  console.log("Adding sources");
-  await Promise.all([
-    sourceCreate(0, "FujiXWeekly", "Goated Recipe Platform"),
-    sourceCreate(1, "CaptnLook", "Cinematic Film Presets")
-  ]);
-}
-
-async function createRecipes() {
-  console.log("Adding Recipes");
-  await recipeCreate(0, "Kodak Gold 200", filmSims[0], {
-    dynamic_range: "DR-Auto",
-    grain: "Strong",
-    ccfx: "Off",
-    ccfx_blue: "Off",
-    white_balance: {
-      style: "Daylight",
-      shift: {
-        red: 4,
-        blue: -5
-      }
-    },
-    highlight: -2,
-    shadow: 1,
-    color: 3,
-    sharpness: -2,
-    noise_reduction: -4,
-    clarity: 0,
-    iso: 6400,
-    exposure_compensation: {
-      min: "2/3",
-      max: "1 1/3"
-    },
-  },
-  sources[0])
-}
+main().catch((err) => console.log(err));
